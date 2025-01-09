be84873744694c7c063e652b55d551bab37bc83d
Dev-DineshYadav <dinesh.yadav@techsierra.in>
2025-01-09 23:12:24 +0530

    Set up custom App and Document components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import InstallPrompt from '../components/InstallPrompt';
+import { CartProvider } from '@/contexts/CartContext';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+      <InstallPrompt
+        appName='E-commerce PWA'
+        description='Install our app for a better experience'
+        icon='/icons/icon-512.png'
+        screenshots={['/screenshots/desktop.png', '/screenshots/mobile.png']}
+      />
+    </CartProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/image1.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="EcommPWA" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
